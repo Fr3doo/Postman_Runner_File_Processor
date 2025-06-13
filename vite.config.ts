5e5b20978fed29eec5ef6811f97255a91065bc88
Fr3doo <134433826+Fr3doo@users.noreply.github.com>
2025-06-14 00:55:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,16 @@ export default defineConfig({
       reporter: ['text', 'lcov'],
       reportsDirectory: 'coverage',
       exclude: [
-        'src/cli/**',             // Scripts CLI non testables ou à faible logique
-        'src/config/**',          // Fichiers de config (ex: app, security)
-        'src/types/**',           // Définitions TypeScript
-        'src/main.tsx',           // Point d’entrée React (souvent trivial)
-        'src/vite-env.d.ts',      // Fichier généré automatiquement
-        '**/*.d.ts',              // Tous les fichiers de type TS
+      'src/cli/**',               // Scripts CLI non testables ou à faible logique
+      'src/config/**',            // Fichiers de config internes (ex: app, security)
+      'src/types/**',             // Définitions TypeScript
+      'src/main.tsx',             // Point d’entrée React (souvent trivial)
+      'src/vite-env.d.ts',        // Fichier généré automatiquement
+      '**/*.d.ts',                // Tous les fichiers de type TS
+      'vite.config.ts',           // Ce fichier-ci
+      'tailwind.config.js',
+      'postcss.config.js',
+      'eslint.config.js',
       ],
     },
   },
