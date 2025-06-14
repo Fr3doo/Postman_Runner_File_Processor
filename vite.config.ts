075c66bead14bd6d58441b2fc68eab9dbd0fd2c4
Fr3doo <134433826+Fr3doo@users.noreply.github.com>
2025-06-14 10:43:38 +0200

    style: format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,16 +15,18 @@ export default defineConfig({
       reporter: ['text', 'lcov'],
       reportsDirectory: 'coverage',
       exclude: [
-      'src/cli/**',               // Scripts CLI non testables ou à faible logique
-      'src/config/**',            // Fichiers de config internes (ex: app, security)
-      'src/types/**',             // Définitions TypeScript
-      'src/main.tsx',             // Point d’entrée React (souvent trivial)
-      'src/vite-env.d.ts',        // Fichier généré automatiquement
-      '**/*.d.ts',                // Tous les fichiers de type TS
-      'vite.config.ts',           // Ce fichier-ci
-      'tailwind.config.js',
-      'postcss.config.js',
-      'eslint.config.js',
+        'src/cli/convert.ts', // Exclu du coverage: script CLI principal
+        'src/cli/index.ts', // Exclu du coverage: point d'entrée CLI
+        'src/cli/**', // Scripts CLI non testables ou à faible logique
+        'src/config/**', // Fichiers de config internes (ex: app, security)
+        'src/types/**', // Définitions TypeScript
+        'src/main.tsx', // Point d’entrée React (souvent trivial)
+        'src/vite-env.d.ts', // Fichier généré automatiquement
+        '**/*.d.ts', // Tous les fichiers de type TS
+        'vite.config.ts', // Ce fichier-ci
+        'tailwind.config.js',
+        'postcss.config.js',
+        'eslint.config.js',
       ],
     },
   },
